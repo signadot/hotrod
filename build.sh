fe1cfdc71070bc98635af426a4262e1b4f28d9be
Anthony Yeh <enisoc@enisoc.dev>
2021-06-03 11:09:31 -0500

    Use Go 1.16 embed for web assets.
    The go build will embed the files so we don't need separate generate
    step.

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-# Embed web assets into the binary.
-go run github.com/mjibson/esc -pkg frontend -o services/frontend/gen_assets.go  -prefix services/frontend/web_assets services/frontend/web_assets
-
 go build -o hotrod

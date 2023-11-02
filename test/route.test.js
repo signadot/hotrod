340520efcf307a1730a928fd49b90aa7b4ffaf51
Anirudh Ramanathan <anirudh@foxish.me>
2023-11-02 16:58:13 -0700

    API Tests Example + GHA (#191)
    * add mocha tests
    
    * add github action to run tests
    
    * test GHA
    
    * npm install
    
    * npm don't fail test
    
    * change to use branch tag for PR
    
    * fix creds
    
    * fix naming
    
    * adding for all services
    
    * add other GHAs
    
    * test change for FE
    
    * fix naming
    
    * should fail test
    
    * hide API key printing
    
    * revert changes to services
    
    * fix names of sandboxes

---
+++ test/route.test.js
@@ -0,0 +1,87 @@
+const request = require('request');
+const assert = require('assert');
+
+const HOST = process.env.ENDPOINT;
+const SIGNADOT_API_KEY = process.env.SIGNADOT_API_KEY;
+
+// Set the default headers for all requests.
+if (SIGNADOT_API_KEY) {
+    console.log("Setting Signadot API key");
+    req = request.defaults({
+        headers: {
+            'signadot-api-key': SIGNADOT_API_KEY,
+        }
+    });
+} else {
+    req = request;
+}
+
+describe("Route Service", function() {
+    it("should return a 200 status code and response body matches the expected JSON structure for /route", function(done) {
+        // Make a request to the /route endpoint with query parameters
+        req.get({
+            url: `${HOST}/route`,
+            qs: {
+                pickup: '123',
+                dropoff: '456'
+            }
+        }, function(error, response, body) {
+            // Assert that the status code is 200
+            assert.equal(response.statusCode, 200);
+
+            // Parse the response body as JSON
+            const responseBody = JSON.parse(body);
+
+            // Assert that the response body has the expected properties and data types
+            assert.ok(responseBody.hasOwnProperty('Pickup'));
+            assert.ok(responseBody.hasOwnProperty('Dropoff'));
+            assert.ok(responseBody.hasOwnProperty('ETA'));
+            assert.strictEqual(typeof responseBody.Pickup, 'string');
+            assert.strictEqual(typeof responseBody.Dropoff, 'string');
+            assert.strictEqual(typeof responseBody.ETA, 'number');
+
+            done();
+        });
+    });
+
+    it("should return a 400 status code when missing required 'pickup' parameter' message for /route", function(done) {
+        // Make a request to the /route endpoint
+        req.get(`${HOST}/route`, function(error, response, body) {
+            // Assert that the status code is 400
+            assert.equal(response.statusCode, 400);
+
+            // Assert that the response body contains the expected message
+            assert.equal(body, "Missing required 'pickup' parameter\n");
+
+            done();
+        });
+    });
+
+    it("should return a 400 status code with 'Missing required 'dropoff' parameter' message for /route", function(done) {
+        // Make a request to the /route endpoint with query parameter
+        req.get(`${HOST}/route?pickup=123`, function(error, response, body) {
+            // Assert that the status code is 400
+            assert.equal(response.statusCode, 400);
+
+            // Assert that the response body contains the expected message
+            assert.equal(body, "Missing required 'dropoff' parameter\n");
+
+            done();
+        });
+    });
+
+    // it("should return a non-negative ETA for /route", function(done) {
+    //     // Make a request to the /route endpoint with query parameters
+    //     req.get({
+    //         url: `${HOST}/route`,
+    //         qs: {
+    //             pickup: '123',
+    //             dropoff: '456'
+    //         }
+    //     }, function(error, response, body) {
+    //         const data = JSON.parse(body);
+    //         assert.ok(data.ETA >= 0);
+    //         done();
+    //     });
+    // });
+});

b5a0478b2140c71dfed342be09b067a02471601d
Daniel De Vera <daniel@signadot.com>
2024-03-05 18:02:44 -0300

    Addition of integration tests

--- services/frontend/server_test.go
+++ services/frontend/server_test.go
@@ -3,6 +3,7 @@ package frontend
 import (
 	"embed"
 	"net/http"
+	"os"
 	"testing"
 )
 
@@ -10,6 +11,11 @@ import (
 var testWebAssetsFS embed.FS
 
 func TestServer(t *testing.T) {
+	if os.Getenv("GOTEST_MANUAL") == "" {
+		t.Skip()
+		return
+	}
+
 	m := http.NewServeMux()
 	m.Handle("/", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ehlo\n"))

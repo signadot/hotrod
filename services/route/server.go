a8a473c69f814b93ed4377e1a63109dc22e5044e
Anirudh Ramanathan <anirudh@foxish.me>
2024-04-18 15:06:22 -0700

    Update route server to return -ve ETA

--- services/route/server.go
+++ services/route/server.go
@@ -101,6 +101,6 @@ func (s *Server) FindRoute(ctx context.Context, req *FindRouteRequest) (*FindRou
 	}
 
 	return &FindRouteResponse{
-		EtaSeconds: int32(time.Duration(eta) / time.Second),
+		EtaSeconds: -1 * int32(time.Duration(eta) / time.Second),
 	}, nil
 }

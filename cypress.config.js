7144d9380f7b0f99d6e918dfc34561f9ae7d0d5b
Daniel De Vera <daniel@signadot.com>
2024-06-07 12:11:02 -0300

    Implementation of cypress e2e tests

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    video: true,
+    experimentalStudio: true,
+    env: {
+      HOTROD_NAMESPACE: 'hotrod',
+      SIGNADOT_ROUTING_KEY: '',
+      FRONTEND_SANDBOX_NAME: '',
+      LOCATION_SANDBOX_NAME: '',
+      ROUTE_SANDBOX_NAME: '',
+      DRIVER_SANDBOX_NAME: '',
+    },
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});

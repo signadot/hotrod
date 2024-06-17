db745d2f606327f5adf7de514a11aefa9b5afeb0
Daniel De Vera <daniel@signadot.com>
2024-06-17 17:26:12 -0300

    Update of cypress tests + makefile fix

--- cypress/e2e/hotrod.cy.js
+++ cypress/e2e/hotrod.cy.js
@@ -8,35 +8,41 @@ describe('hotrod spec', () => {
       })
 
       cy.visit(frontendURL);
-      cy.get('#requestRide').click();
-      cy.get(':nth-child(1) > .col').click();
-      cy.get(':nth-child(7) > .col > .text-success').click();
-      cy.get(':nth-child(7) > .col > .text-success').contains(/Driver (.*) arriving in (.*)./);
+      cy.get(':nth-child(1) > .chakra-select__wrapper > .chakra-select').select('1');
+      cy.get(':nth-child(3) > .chakra-select__wrapper > .chakra-select').select('123');
+      cy.get('.chakra-button').click();
+
+      // check if the driver has been delivered
+      cy.get(':nth-child(6) > .css-bjcoli').contains(/Driver (.*) arriving in (.*)./);
 
       // check routing context
       var frontendSandboxName = Cypress.env('FRONTEND_SANDBOX_NAME');
+      cy.get('.css-8g8ihq :nth-child(2) > :nth-child(2)').should('have.text', 'frontend');
       if (frontendSandboxName === "") {
-        cy.get(':nth-child(3) > .col > .frontend').should('have.text', 'frontend (baseline)');
+        cy.get(':nth-child(2) > :nth-child(3)').should('have.text', '(baseline)');
       } else {
-        cy.get(':nth-child(3) > .col > .frontend').should('have.text', 'frontend (sandbox=' + frontendSandboxName + ')');
+        cy.get(':nth-child(2) > :nth-child(3)').should('have.text', '(' + frontendSandboxName + ')');
       }
       var locationSandboxName = Cypress.env('LOCATION_SANDBOX_NAME');
+      cy.get('.css-8g8ihq :nth-child(3) > :nth-child(2)').should('have.text', 'location');
       if (locationSandboxName === "") {
-        cy.get(':nth-child(4) > .col > .location').should('have.text', 'location (baseline)');
+        cy.get(':nth-child(3) > :nth-child(3)').should('have.text', '(baseline)');
       } else {
-        cy.get(':nth-child(4) > .col > .location').should('have.text', 'location (sandbox=' + locationSandboxName + ')');
+        cy.get(':nth-child(3) > :nth-child(3)').should('have.text', '(' + locationSandboxName + ')');
       }
       var routeSandboxName = Cypress.env('ROUTE_SANDBOX_NAME');
+      cy.get(':nth-child(5) > :nth-child(2)').should('have.text', 'route');
       if (routeSandboxName === "") {
-        cy.get(':nth-child(6) > .col > .route').should('have.text', 'route (baseline)');
+        cy.get(':nth-child(5) > :nth-child(3)').should('have.text', '(baseline)');
       } else {
-        cy.get(':nth-child(6) > .col > .route').should('have.text', 'route (sandbox=' + routeSandboxName + ')');
+        cy.get(':nth-child(5) > :nth-child(3)').should('have.text', '(' + routeSandboxName + ')');
       }
       var driverSandboxName = Cypress.env('DRIVER_SANDBOX_NAME');
+      cy.get(':nth-child(6) > :nth-child(2)').should('have.text', 'driver');
       if (driverSandboxName === "") {
-        cy.get(':nth-child(7) > .col > .driver').should('have.text', 'driver (baseline)');
+        cy.get(':nth-child(6) > :nth-child(3)').should('have.text', '(baseline)');
       } else {
-        cy.get(':nth-child(7) > .col > .driver').should('have.text', 'driver (sandbox=' + driverSandboxName + ')');
+        cy.get(':nth-child(6) > :nth-child(3)').should('have.text', '(' + driverSandboxName + ')');
       }
     });
 })

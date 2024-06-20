634c85a541ca7a3a2ab8f944d60ba604eb7fe311
Scott Cotton <scott@mindowl.com>
2024-06-20 10:51:59 +0200

    makefile-fix-fix

---
+++ tag-release.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+
+set -ex
+
+
+RELEASE_TAG=$1
+
+if [ -z ${RELEASE_TAG} ] ; then
+	echo "usage $0 <release-tag>"
+	exit 1
+fi
+
+if [ ! -d k8s/base ]; then
+	echo wrong dir
+	exit 1
+fi
+
+#
+# check for changes before editing images
+#
+git diff-index  --exit-code HEAD
+
+(cd k8s/base && kustomize edit set image signadot/hotrod:${RELEASE_TAG})
+
+# do we need to commit?
+set +e
+git diff-index  --exit-code HEAD
+diffCode=$?;
+set -e
+
+if [ ${diffCode} -eq 1 ] ; then 
+	git commit -m tag-release-${RELEASE_TAG} k8s/base
+	echo commited tag-release-${RELEASE_TAG}
+elif [ ${diffCode} -eq 0 ] ; then
+	echo ${RELEASE_TAG} already committed
+else
+	echo "git diff-index failed"
+	exit 1
+fi
+
+
+# in any event, make sure we've tagged it locally and in-repo
+git tag -a -f -m release-${RELEASE_TAG} ${RELEASE_TAG}
+
+git push origin -f ${RELEASE_TAG}
+

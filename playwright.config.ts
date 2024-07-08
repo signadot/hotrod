815c3829c56d7af85bece642a9619108d4ccef9b
David Orozco <orozco.davidj02@gmail.com>
2024-07-08 09:38:22 -0500

    print baseurl

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,6 +13,8 @@ import { defineConfig, devices } from '@playwright/test';
 
 const BASE_URL = `http://frontend.${process.env.HOTROD_NAMESPACE}:8080`;
 
+console.log({ BASE_URL });
+
 export default defineConfig({
   testDir: './playwright-tests',
   /* Run tests in files in parallel */
@@ -20,7 +22,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 0: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */

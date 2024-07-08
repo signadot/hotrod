a117a3879d9594792be6528d44c08fb61ba549a0
David Orozco <orozco.davidj02@gmail.com>
2024-07-08 11:00:22 -0500

    fix routing key

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,8 +13,6 @@ import { defineConfig, devices } from "@playwright/test";
 
 const BASE_URL = `http://frontend.${process.env.HOTROD_NAMESPACE}:8080`;
 
-console.log(process.env.ROUTING_KEY)
-
 export default defineConfig({
 	testDir: "./playwright-tests",
 	/* Run tests in files in parallel */
@@ -37,7 +35,7 @@ export default defineConfig({
 
 		video: "on",
 		extraHTTPHeaders: {
-			baggage: `sd-routing-key=${process.env.ROUTING_KEY}`,
+			baggage: `sd-routing-key=${process.env.SIGNADOT_ROUTING_KEY}`,
 		},
 	},
 
@@ -49,7 +47,7 @@ export default defineConfig({
 				...devices["Desktop Chrome"],
 				baseURL: BASE_URL,
 				extraHTTPHeaders: {
-					baggage: `sd-routing-key=${process.env.ROUTING_KEY}`,
+					baggage: `sd-routing-key=${process.env.SIGNADOT_ROUTING_KEY}`,
 				},
 			},
 		},

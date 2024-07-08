1d79c84b573ceb100fda0bcf8e48d8263ecd99c8
David Orozco <orozco.davidj02@gmail.com>
2024-07-08 08:43:28 -0500

    no open reporter when finished

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,6 +10,8 @@ import { defineConfig, devices } from '@playwright/test';
 /**
  * See https://playwright.dev/docs/test-configuration.
  */
+
+console.log(process.env);
 export default defineConfig({
   testDir: './playwright-tests',
   /* Run tests in files in parallel */
@@ -21,7 +23,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [['html', { open: 'never' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */

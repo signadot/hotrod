019d9f389501da920c86ce20b8007b5aa3a4e374
David Orozco <orozco.davidj02@gmail.com>
2024-12-13 15:28:02 -0500

    move logs to sidebar and make map live

--- services/frontend/react_app/src/App.tsx
+++ services/frontend/react_app/src/App.tsx
@@ -1,13 +1,39 @@
 import './App.css'
 import {HomePage} from "./pages/home.tsx";
 import {SessionProvider} from "./context/sessionContext/context.tsx";
+import {ChakraProvider, extendTheme} from "@chakra-ui/react";
 
 function App() {
-  return (
-      <SessionProvider>
-        <HomePage />
-      </SessionProvider>
-  )
+
+    const components = {
+        Drawer: {
+            variants: {
+                aside: {
+                    overlay: {
+                        pointerEvents: 'none',
+                        background: 'transparent',
+                    },
+                    dialogContainer: {
+                        pointerEvents: 'none',
+                        background: 'transparent',
+                    },
+                    dialog: {
+                        pointerEvents: 'auto',
+                    },
+                },
+            },
+        },
+    };
+
+    const theme = extendTheme({components});
+
+    return (
+        <ChakraProvider theme={theme}>
+            <SessionProvider>
+                <HomePage/>
+            </SessionProvider>
+        </ChakraProvider>
+    )
 }
 
 export default App
